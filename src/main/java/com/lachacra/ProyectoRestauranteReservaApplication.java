302b5659fe5cc0fb6264e73e0dd08b50c11a2622
Jair Arteaga <jair_55_88@hotmail.com>
2020-06-25 13:10:06 -0500

    probando

---
+++ src/main/java/com/lachacra/ProyectoRestauranteReservaApplication.java
@@ -0,0 +1,13 @@
+package com.lachacra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoRestauranteReservaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoRestauranteReservaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lachacra/ProyectoRestauranteReservaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lachacra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoRestauranteReservaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
